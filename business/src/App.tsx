8d08cd031f3a2b69c62fba1ecd03b5d8555e7994
Ekaterina <kattren990703@gmail.com>
2025-03-18 12:48:06 +0500

    first commit

---
+++ business/src/App.tsx
@@ -0,0 +1,15 @@
+import './index.css';
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-[100px] text-center underline">
+          Hello, Tailwind!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App

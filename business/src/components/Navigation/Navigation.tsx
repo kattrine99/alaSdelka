ff9e073b01959ed392a07cd8ad6260f983ba188d
Ekaterina <kattren990703@gmail.com>
2025-03-28 15:36:29 +0500

    added into git ignore

--- business/src/components/Navigation/Navigation.tsx
+++ business/src/components/Navigation/Navigation.tsx
@@ -8,7 +8,7 @@ interface NavLinksProps {
 
 export const NavLinks: React.FC<NavLinksProps> = ({ links, className = "", linkClassName = "" }) => {
     return (
-        <nav className={`flex flex-wrap gap-8.5 font-medium${className}`}>
+        <nav className={`flex flex-wrap gap-x-8.5 font-medium${className}`}>
             {links.map((link, idx) => (
                 <Applink key={idx} to={link.to} className={`text-[#232323] ${linkClassName}`}>
                     {link.label}

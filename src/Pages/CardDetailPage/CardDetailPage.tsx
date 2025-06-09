8e793df6bb62dff9ef30218f2db2d3271b45e073
Dmitriy Lijin <dmitriyl1899@gmail.com>
2025-06-09 15:17:32 +0500

    fix adaptive for card detail

--- src/Pages/CardDetailPage/CardDetailPage.tsx
+++ src/Pages/CardDetailPage/CardDetailPage.tsx
@@ -108,10 +108,10 @@ export const CardDetailPage = () => {
 
             ) :
                 (
-                    <div className=''>
+                    <div className='container mx-auto px-3 md:px-0 pt-6'>
                         <Breadcrumbs category={category} title={card.title} />
                         <div className='flex flex-wrap xl:flex-nowrap'>
-                            <div className='flex flex-col w-full'>
+                            <div className='flex flex-col w-full lg:w-3/4 justify-center'>
                                 <Heading className="text-[24px] md:text-4xl font-inter leading-10 font-bold mt-6 mb-3.5" text={card.title} level={2} />
                                 <div className='flex flex-col gap-2 mb-[15px]'>
                                     <Paragraph className='font-inter font-bold text-[#363636] text-[16px]'>ID {card.id}</Paragraph>
